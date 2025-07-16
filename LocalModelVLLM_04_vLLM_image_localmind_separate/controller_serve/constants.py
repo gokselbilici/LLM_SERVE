aaaebc6a65e888fadef6d12f56b7c9d13610b0fd
Göksel Bilici <goksel.bilicii@gmail.com>
2025-07-16 16:20:26 +0300

    4.0

--- LocalModelVLLM_04_vLLM_image_localmind_separate/controller_serve/constants.py
+++ LocalModelVLLM_04_vLLM_image_localmind_separate/controller_serve/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # The base URL for internal container-to-container communication
-LLM_BACKEND_URL = os.getenv("LLM_API_HOST", "http://llm_engine:8000")
+LLM_BACKEND_URL = os.getenv("LLM_API_HOST", "http://vllm_openai_container:8000")
 
 # Optional: default system prompt if used
 MAIN_SYSTEM_PROMPT = (
